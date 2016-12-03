d4af5b63e01f56ce1696aa0899f9b341886843e4
brianfu <Fu.Brian@outlook.com>
2016-12-02 17:58:16 -0800

    5:58

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+##CMPT 120 D100; 16-3
+##Jack 'Jryzkns' Zhou
+##Brian Fu
+##Daniel Lee
+##Final Assignment - Planets, Aliens, and Explosions
 
 import Current
 
 import time
-import
+import random
+import turtle
+
+
+
+
